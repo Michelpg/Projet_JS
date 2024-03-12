10202aeeb84436afaa546c864dd27bfd266107bf
michel <michel.peng@hotmail.com>
2024-03-12 13:55:37 +0100

    ajout de la page connexion

--- page.js
+++ page.js
@@ -2,4 +2,6 @@ function commencerJeu() {
     alert("Le jeu va commencer !");
     window.location.href = "page_question.html";
   }
-  
+  function redirigerVersPageConnexion() {
+    window.location.href = "page_connexion.html"; 
+  }
