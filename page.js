a5eb96379fd36e41e771169f5586b6c076a29810
michel <michel.peng@hotmail.com>
2024-03-26 13:45:25 +0100

    creation dossier front et Back

--- page.js
+++
@@ -1,7 +0,0 @@
-function commencerJeu() {
-    alert("Le jeu va commencer !");
-    window.location.href = "page_question.html";
-  }
-  function redirigerVersPageConnexion() {
-    window.location.href = "page_connexion.html"; 
-  }
