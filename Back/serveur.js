da45a990f4db0e3079361e7e8b6498b8a831e43e
michel <michel.peng@hotmail.com>
2024-04-03 10:52:34 +0200

    Inscription fonctionel

--- Back/serveur.js
+++ Back/serveur.js
@@ -6,10 +6,28 @@ const PORT = 3000;
 
 
 
+const addInscription = require("./inscription.inscription");
 app.use(cors({ origin: "*", methods: "GET,HEAD,PUT,PATCH,POST,DELETE" }));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+app.post("/inscription/add", (req, res) => {
+  const { nom, mail, mdp } = req.body;
+  console.log({ nom, mail, mdp });
+  addInscription({ nom, mail, mdp })
+    .then((affectedRows) => {
+      res.status(200).json({ "Affected rows":  affectedRows });
+    })
+    .catch((error) => {
+      res.status(500).json({ "error": "Failed to insert data" });
+    });
+});
+
+
+
 
 
 app.listen(PORT, function (err) {
---
+++ Front/inscription.js
@@ -0,0 +1,18 @@
+const addInscription = () => {
+  let nom = document.getElementById("nom_utilisateur").value;
+  let mail = document.getElementById("email").value;
+  let mdp = document.getElementById("mdp").value;
+  const inscription = { "nom": nom, "mail": mail, "mdp": mdp };
+
+  
+  console.log(JSON.stringify(inscription));
+
+  axios
+    .post("http://localhost:3000/inscription/add", inscription)
+    .then((res) => {
+      console.log(res.data);
+    })
+    .catch((err) => {
+      console.error(err.message);
+    });
+};
